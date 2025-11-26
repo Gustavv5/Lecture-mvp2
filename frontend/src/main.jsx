85fcba8add89312cce4c50d9badd5d85decd107c
Gustav <gustav.greger@gmail.com>
2025-11-26 19:49:56 +0100

    Commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,3 +11,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <AccessGate>
+    <App />
+  </AccessGate>
+);
